c6ce7e7560172599be75aaa67e5af2f4399d1493
John Chrosniak <jlc9wr@virginia.edu>
2022-02-25 13:43:12 -0500

    initial commit

---
+++ speed_trap/speed_trap/ego_velocity_estimator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Vector3, Point
+from datetime import datetime
+
+previous_timestamp = None
+previous_pose  = Point()
+is_initialized = False
+velocity_publisher = None
+
+def ego_velocity_estimator():
+    global velocity_publisher
+    rospy.init_node('ego_velocity_estimator')
+    velocity_publisher  = rospy.Publisher("ego_velocity", Vector3, queue_size=20)
+    ego_odom  = rospy.Subscriber("odom", Odometry, odom_callback)
+    rospy.spin()
+    
+
+def odom_callback(msg : Odometry):
+    global previous_timestamp, previous_pose, is_initialized, velocity_publisher
+    if not is_initialized:
+        # print(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)))
+        previous_timestamp = datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9)
+        previous_pose = msg.pose.pose.position
+        is_initialized = True
+        return
+    current_time = datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9)
+    time_difference = (current_time - previous_timestamp).total_seconds()
+    # print(time_difference)
+    ego_velocity = Vector3()
+    ego_velocity.x = (msg.pose.pose.position.x - previous_pose.x) / time_difference
+    ego_velocity.y = (msg.pose.pose.position.y - previous_pose.y) / time_difference
+    ego_velocity.z = (msg.pose.pose.position.z - previous_pose.z) / time_difference
+    previous_timestamp = datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9)
+    previous_pose = msg.pose.pose.position
+    velocity_publisher.publish(ego_velocity)
+
+if __name__ == '__main__':
+    ego_velocity_estimator()
---
+++ speed_trap/speed_trap/ticket_issuer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from turtle import speed
+import rospy
+# from lgsvl_msgs.msg import DetectedRadarObjectArray
+from aipd_msgs.msg import DetectedObjectArray
+from aipd_msgs.msg import Ticket
+from geometry_msgs.msg import Vector3
+from datetime import datetime
+import numpy as np
+
+ticket_pub = None
+issued_tickets = set()
+speed_limit = 10  # m/s
+ego_velocity = Vector3()
+instance_tracking_pose = dict()
+instance_tracking_time = dict()
+
+def ticket_issuer():
+    global ticket_pub
+    rospy.init_node('ticket_issuer')
+    ticket_pub  = rospy.Publisher("tickets", Ticket, queue_size=20)
+    ego_velocity_sub = rospy.Subscriber("ego_velocity", Vector3, ego_velocity_callback)
+    objects  = rospy.Subscriber("detected_objects", DetectedObjectArray, detected_object_callback)
+    rospy.spin()
+    
+
+def detected_object_callback(msg : DetectedObjectArray):
+    global ticket_pub, issued_tickets, speed_limit, instance_tracking_pose, instance_tracking_time
+    objects = msg.objects
+    for object in objects:
+        if not object.is_vehicle or object.id in issued_tickets:
+            continue
+        if object.id not in instance_tracking_pose:
+            instance_tracking_pose[object.id] = object.pose
+            instance_tracking_time[object.id] = datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9)
+            continue
+        object_velocity_x = (object.pose.x - instance_tracking_pose[object.id].x) / (datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9) - instance_tracking_time[object.id]).total_seconds()
+        object_velocity_y = (object.pose.y - instance_tracking_pose[object.id].y) / (datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9) - instance_tracking_time[object.id]).total_seconds()
+        object_velocity_z = (object.pose.z - instance_tracking_pose[object.id].z) / (datetime.fromtimestamp(int(str(msg.header.stamp.secs) + str(msg.header.stamp.nsecs)) / 1e9) - instance_tracking_time[object.id]).total_seconds()
+        object_velocity = np.linalg.norm([object_velocity_x, object_velocity_y, object_velocity_z])
+        if (object_velocity > speed_limit):
+            issued_tickets.add(object.id)
+            ticket = Ticket()
+            ticket.id = object.id
+            ticket.velocity = object_velocity
+            ticket.violation_time = msg.header.stamp
+            ticket_pub.publish(ticket)
+            print("Ticket issued at", ticket.violation_time, "to vehicle", ticket.id, "for moving at", ticket.velocity, "m/s in a", speed_limit, "m/s zone." )
+
+def ego_velocity_callback(msg : Vector3):
+    global ego_velocity
+    ego_velocity = msg
+
+if __name__ == '__main__':
+    ticket_issuer()
